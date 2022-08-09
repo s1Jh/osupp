edcc05884e6210540fc8638962a0051bb66a97b8
s1Jh <s1Jh.199@gmail.com>
2022-08-10 01:33:10 +0200

    Added forgotten files...

---
+++ system/audio/AudioUtil.cpp
@@ -0,0 +1,168 @@
+/*******************************************************************************
+ * Copyright (c) 2022 sijh (s1Jh.199[at]gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+#include "AudioUtil.hpp"
+
+#include "Log.hpp"
+
+NS_BEGIN
+
+FFmpegCtx OpenFFmpegContext(const std::filesystem::path &path)
+{
+	FFmpegCtx ctx;
+
+	int err = 0;
+
+	auto invalidate = [&](const std::string& message = "") {
+		if (err != 0) {
+			char buf[64];
+			av_make_error_string(buf, 64, err);
+			log::error("Failed creating a FFMPEG context: ", message, ' ', buf);
+		}
+		FreeFFmpegContext(ctx);
+		ctx.valid = false;
+		return ctx;
+	};
+
+	if ((err = avformat_open_input(&ctx.format, path.c_str(), nullptr, 0)) != 0) {
+		return invalidate("Error opening file.");
+	}
+
+	avformat_find_stream_info(ctx.format, nullptr);
+
+	for(size_t i = 0; i < ctx.format->nb_streams; ++i) {
+		if(ctx.format->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+			ctx.audioStreamIndex = (int)i;
+			break;
+		}
+	}
+	if(ctx.audioStreamIndex == -1) {
+		avformat_close_input(&ctx.format);
+		return invalidate("None of the available streams are audio streams");
+	}
+
+	ctx.codec = avcodec_find_decoder(ctx.format->streams[ctx.audioStreamIndex]->codecpar->codec_id);
+	if (ctx.codec == nullptr) {
+		avformat_close_input(&ctx.format);
+		return invalidate("Decoder not found. The codec is not supported.");
+	}
+
+	ctx.codecCtx = avcodec_alloc_context3(ctx.codec);
+	if (ctx.codecCtx == nullptr) {
+		avformat_close_input(&ctx.format);
+		return invalidate("Could not allocate a decoding context.");
+	}
+
+	if ((err = avcodec_parameters_to_context(ctx.codecCtx, ctx.format->streams[ctx.audioStreamIndex]->codecpar)) != 0) {
+		avcodec_close(ctx.codecCtx);
+		avcodec_free_context(&ctx.codecCtx);
+		avformat_close_input(&ctx.format);
+		return invalidate();
+	}
+
+	ctx.codecCtx->request_sample_fmt = av_get_alt_sample_fmt(ctx.codecCtx->sample_fmt, 0);
+
+	if ((err = avcodec_open2(ctx.codecCtx, ctx.codec, nullptr)) != 0) {
+		avcodec_close(ctx.codecCtx);
+		avcodec_free_context(&ctx.codecCtx);
+		avformat_close_input(&ctx.format);
+		return invalidate();
+	}
+
+	ctx.frame = nullptr;
+	if ((ctx.frame = av_frame_alloc()) == nullptr) {
+		avcodec_close(ctx.codecCtx);
+		avcodec_free_context(&ctx.codecCtx);
+		avformat_close_input(&ctx.format);
+		return invalidate();
+	}
+
+	ctx.packet = av_packet_alloc();
+
+	ctx.sampleRate = ctx.codecCtx->sample_rate;
+
+	return ctx;
+}
+
+size_t IterateFFmpegFrames(FFmpegCtx &ctx, size_t number, const std::function<bool(FFmpegCtx &)>& func)
+{
+	int err;
+	size_t read = 0;
+
+	while ((err = av_read_frame(ctx.format, ctx.packet)) != AVERROR_EOF && read < number) {
+		if(err != 0) {
+			// Something went wrong.
+			break; // Don't return, so we can clean up nicely.
+		}
+		// Does the packet belong to the correct stream?
+		if(ctx.packet->stream_index != ctx.audioStreamIndex) {
+			// Free the buffers used by the frame and reset all fields.
+			av_packet_unref(ctx.packet);
+			continue;
+		}
+		// We have a valid packet => send it to the decoder.
+		if((err = avcodec_send_packet(ctx.codecCtx, ctx.packet)) == 0) {
+			// The packet was sent successfully. We don't need it anymore.
+			// => Free the buffers used by the frame and reset all fields.
+			av_packet_unref(ctx.packet);
+		} else {
+			// Something went wrong.
+			// EAGAIN is technically no error here but if it occurs we would need to buffer
+			// the packet and send it again after receiving more frames. Thus we handle it as an error here.
+			break; // Don't return, so we can clean up nicely.
+		}
+
+		// Receive and handle frames.
+		// EAGAIN means we need to send before receiving again. So thats not an error.
+
+		while((err = avcodec_receive_frame(ctx.codecCtx, ctx.frame)) == 0) {
+			if (!func(ctx))
+				return read;
+
+			av_frame_unref(ctx.frame);
+		}
+
+		if(err != AVERROR(EAGAIN)) {
+			// Not EAGAIN => Something went wrong.
+			break; // Don't return, so we can clean up nicely.
+		}
+
+		read++;
+	}
+	return read;
+}
+bool IsPlanar(AVSampleFormat fmt)
+{
+	return av_sample_fmt_is_planar(fmt);
+}
+
+void FreeFFmpegContext(FFmpegCtx &ctx)
+{
+	log::debug("Destroying context ", &ctx);
+	av_frame_free(&ctx.frame);
+	avformat_free_context(ctx.format);
+	avcodec_free_context(&ctx.codecCtx);
+	av_packet_free(&ctx.packet);
+}
+
+
+NS_END
+
---
+++ system/audio/AudioUtil.hpp
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright (c) 2022 sijh (s1Jh.199[at]gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <functional>
+
+#include "define.hpp"
+#include "Audio.hpp"
+#include "Log.hpp"
+#include "Math.hpp"
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+#include <libavformat/avformat.h>
+}
+
+NS_BEGIN
+
+struct FFmpegCtx {
+	bool valid{true};
+
+	int audioStreamIndex{-1};
+	const AVCodec* codec{nullptr};
+	AVPacket* packet{nullptr};
+	AVFrame* frame{nullptr};
+	AVFormatContext *format{nullptr};
+	AVCodecContext* codecCtx{nullptr};
+
+	int sampleRate{0};
+};
+
+
+FFmpegCtx OpenFFmpegContext(const std::filesystem::path& path);
+size_t IterateFFmpegFrames(FFmpegCtx& context, size_t number, const std::function<bool(FFmpegCtx&)>&);
+void FreeFFmpegContext(FFmpegCtx& ctx);
+
+bool IsPlanar(AVSampleFormat fmt);
+
+template <typename SampleT>
+void ExtractFFmpegSamplesAppend(FFmpegCtx& ctx, std::vector<SampleT>& read)
+{
+	const SampleFormat fmt = SampleInfo<SampleT>::format;
+
+	auto extractDataFromStream = [&](size_t channel, size_t offset) -> float {
+		auto avFormat = (AVSampleFormat)ctx.frame->format;
+		auto **data = ctx.frame->extended_data;
+
+		uint8_t *stream;
+		if (!IsPlanar(avFormat)) {
+			// Format is interleaved
+			stream = data[0];
+			offset = offset * ctx.frame->channels + channel;
+		} else {
+			// Format is planar
+			stream = data[channel];
+		}
+
+		auto convertToRange = []<typename T>(T value) -> float {
+			auto converted = float(value) / float(std::numeric_limits<T>::max());
+
+			return converted;
+		};
+
+		switch (avFormat) {
+		case AVSampleFormat::AV_SAMPLE_FMT_U8:
+		case AVSampleFormat::AV_SAMPLE_FMT_U8P: {
+			auto value = reinterpret_cast<uint8_t*>(stream)[offset];
+			auto signedValue = int8_t(int(value) - 127);
+			return convertToRange(signedValue);
+		}
+		case AVSampleFormat::AV_SAMPLE_FMT_S16:
+		case AVSampleFormat::AV_SAMPLE_FMT_S16P: {
+			auto value = reinterpret_cast<int16_t*>(stream)[offset];
+			return convertToRange(value);
+		}
+		case AVSampleFormat::AV_SAMPLE_FMT_S32:
+		case AVSampleFormat::AV_SAMPLE_FMT_S32P: {
+			auto value = reinterpret_cast<int32_t*>(stream)[offset];
+			return convertToRange(value);
+		}
+		case AVSampleFormat::AV_SAMPLE_FMT_S64:
+		case AVSampleFormat::AV_SAMPLE_FMT_S64P: {
+			auto value = reinterpret_cast<int64_t*>(stream)[offset];
+			return convertToRange(value);
+		}
+
+		case AVSampleFormat::AV_SAMPLE_FMT_FLT:
+		case AVSampleFormat::AV_SAMPLE_FMT_FLTP:
+			return reinterpret_cast<float*>(stream)[offset];
+
+		case AVSampleFormat::AV_SAMPLE_FMT_DBL:
+		case AVSampleFormat::AV_SAMPLE_FMT_DBLP:
+			return (float)reinterpret_cast<double*>(stream)[offset];
+
+		default:
+			log::error("Unknown sample format: ", avFormat);
+			return 0;
+		}
+	};
+
+	/**
+	 * Converts a float value in range [1;-1] to a unsigned 8 or 16 bit integer.
+	 */
+	auto crushData = [&](float dataIn) -> auto {
+		if constexpr ((fmt & SampleFormat::Type) == SampleFormat::Type8) {
+			// Crush data to uint8_t
+			return uint8_t(std::ceil((dataIn + 1.0f) / 2.0f * std::numeric_limits<uint8_t>::max()));
+		} else if constexpr ((fmt & SampleFormat::Type) == SampleFormat::Type16) {
+			// Crush data to int16_t
+			return int16_t(dataIn * std::numeric_limits<int16_t>::max());
+		} else {
+			WRAP_CONSTEXPR_ASSERTION("Incorrect sample format type");
+		}
+	};
+
+	for (int i = 1; i < ctx.frame->nb_samples; i++) {
+		if constexpr(bool(fmt & SampleFormat::IsStereo)) {
+			const size_t leftId = Min(1, ctx.frame->channels);
+			const size_t rightId = Min(2, ctx.frame->channels);
+
+			float left = extractDataFromStream(leftId, i);
+			float right = extractDataFromStream(rightId, i);
+			read.emplace_back(SampleT{crushData(left), crushData(right)});
+		}
+		else {
+			const size_t centerId = 1;
+
+			float mono = extractDataFromStream(centerId, i);
+			read.emplace_back(crushData(mono));
+		}
+	}
+}
+
+template <typename SampleT>
+std::vector<SampleT> ExtractFFmpegSamples(FFmpegCtx& ctx) {
+	std::vector<SampleT> read;
+	ExtractFFmpegSamplesAppend(ctx, read);
+	return read;
+}
+
+NS_END
